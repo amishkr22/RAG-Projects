c1ee9cba8cc787d8f5004941382cb8a98c514206
amishkr22 <amishkumar1028@gmail.com>
2025-03-11 12:06:04 +0530

    local package installed

---
+++ 01_Basic_RAG/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'basic_rag',
+    version = "0.0.0",
+    author = 'Amish Nayar',
+    author_email='amishkumar1028@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
